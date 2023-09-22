94b50eb1ff3f0d928004968b95bf6778c6a3b2ca
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-09-22 13:49:54 -0300

    about and members

---
+++ src/components/AboutTheBand/index.js
@@ -0,0 +1,74 @@
+import { useState } from "react";
+import Member from "../Member";
+
+const AboutTheBand = () => {
+
+  const [membro1, setMembro1] = useState("hidden");
+  const [membro2, setMembro2] = useState("hidden");
+  const [membro3, setMembro3] = useState("hidden");
+
+  const membros = [
+    {
+      nome: "Zaka Kappel",
+      função: "Vocalista",
+      bio: "Zaka tem 25 anos e começou a banda em 2019, com seu background de surf e skate, traz essa vibe para os palcos.",
+      view: membro1,
+      setView: setMembro1,
+      id: 1,
+    },
+    {
+      nome: "Nome",
+      função: "Função",
+      bio: "Breve biografia...Breve biografia...Breve biografia...Breve biografia...Breve biografia...",
+      view: membro2,
+      setView: setMembro2,
+      id: 2,
+    },
+    {
+      nome: "Nome",
+      função: "Função",
+      bio: "Breve biografia...Breve biografia...Breve biografia...Breve biografia...Breve biografia...Breve biografia...",
+      view: membro3,
+      setView: setMembro3,
+      id: 3,
+    },
+  ];
+
+  function changeView(id) {
+    membros.forEach((mem) => {
+      console.log(id);
+      if (mem.id === id) {
+        if(mem.view === ""){
+            mem.setView("hidden")
+        } else{
+            mem.setView("");
+        }
+      } else{
+        mem.setView("hidden")
+      }
+    });
+  }
+
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <h2 className="py-5 font-body text-xl lg:py-10">Conheça a banda:</h2>
+      <div className="flex flex-col lg:flex-row justify-center items-center gap-3 lg:gap-20">
+        {membros.map((membro) => {
+          return (
+            <Member
+              name={membro.nome}
+              func={membro.função}
+              bio={membro.bio}
+              view={membro.view}
+              key={membro.id}
+              id={membro.id}
+              changeView={changeView}
+            />
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default AboutTheBand;
---
+++ src/components/Member/index.js
@@ -0,0 +1,33 @@
+import membro from "../../assets/imgs/membro da banda.jpg";
+
+const Member = ({ name, func, bio, view, id, changeView }) => {
+  function changeV() {
+    changeView(id);
+  }
+
+  return (
+    <div className="flex flex-col items-center lg:relative">
+      <img
+        className="rounded-full w-40 align-super hover:cursor-pointer"
+        src={membro}
+        alt="membro da banda"
+        onClick={() => changeV()}
+      />
+
+      <section
+        className={`bg-slate-100 mx-2 rounded-md lg:absolute lg:top-44 lg:-left-16 lg:w-72 ${view} `}
+      >
+        <header className="flex justify-around items-center pt-3 px-3 pb-3">
+          <h2 className="font-body text-xl">{name}</h2>
+          <span className="font-body text-md text-stone-500">{func}</span>
+        </header>
+
+        <div className="h-1 bg-white rounded-sxl"></div>
+        <p className="p-1 text-sm">{bio}</p>
+      </section>
+
+    </div>
+  );
+};
+
+export default Member;
