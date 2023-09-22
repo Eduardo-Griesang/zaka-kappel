a7b4696538e8fb2bbe3fd9cbc8eb5f79de16e252
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-09-21 23:53:13 -0300

    title

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+
+const Header = () => {
+    return (
+        <div className="flex justify-center items-center bg-emerald-400 p-6 h-screen">
+            <h1 className="font-bold text-zinc-100 text-9xl
+            font-title ">
+                Zaka Kappel
+            </h1>
+        </div>
+    )
+}
+
+export default Header
