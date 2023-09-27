ad7611bc8906cfb2e9cd0ccef0d1bcfdbfff345d
Eduardo Griesang <eduardogriesang1@gmail.com>
2023-09-27 19:44:41 -0300

    lazy load

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,14 +1,18 @@
 import Header from "../../components/Header";
 import Paralax from "../../components/Paralax";
-import Music from "../../components/Music";
 import Footer from "../../components/Footer";
+import React, { Suspense } from "react";
+
+const Music = React.lazy(() => import('../../components/Music'))
 
 const LandingPage = () => {
   return (
     <>
       <Header />
       <Paralax />
-      <Music />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Music></Music>
+      </Suspense>
       <Footer />
     </>
   );
